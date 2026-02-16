f922bad477e675b1905daaf4715b107a1b889a5a
Ajay Rawat <ajayrawat0706@gmail.com>
2026-02-16 19:48:05 +0530

    initial commit

---
+++ src/test/java/com/ajay/fundbridge/FundbridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajay.fundbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundbridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
